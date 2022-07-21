f8e8e18f67072d0fd75dc807a5834bf092b53a76
devphase <87848253+devphase01@users.noreply.github.com>
2022-07-21 19:53:35 +0300

    Delete index_20220721165312.js

--- .history/client/src/components/index_20220721165312.js
+++
@@ -1,8 +0,0 @@
-export { default as Navbar } from './Navbar/Navbar';
-export { default as ProductCard } from './ProductCard/ProductCard';
-export { default as Sidebar } from './Sidebar/Sidebar';
-export { default as ProductContainer } from './ProductContainer/ProductContainer';
-export { default as CartProductItem } from './CartProductItem/CartProductItem';
-export { default as CartProducts } from './CartProducts/CartProducts';
-export { default as InputForm } from './InputForm/InputForm';
-export { default as Layout } from './Layout/Layout';
